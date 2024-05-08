9a3eea42c864d6e3a4da7d21e7362509001fe42b
Kai Feinberg <kaiwfeinberg@gmail.com>
2024-05-07 20:04:05 -0500

    iterate over active strategies

--- packages/nextjs/app/manage/page.tsx
+++ packages/nextjs/app/manage/page.tsx
@@ -10,13 +10,15 @@
 import { DataTable } from "@/components/DataTable";
 import { ColumnDef } from "@tanstack/react-table";
 import { Button } from "@/components/ui/button";
-import {XSquare,Pencil } from "lucide-react";
+import { XSquare, Pencil } from "lucide-react";
 import React from "react";
 import PageTitle from "@/components/PageTitle";
 import { TokenData } from "~~/types/customTypes"; // token data type defined in customTypes.ts
 import { Strategy } from "~~/types/customTypes"; // strategy type defined in customTypes.ts
 import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
+import  getStrategyData from "~~/hooks/scaffold-eth/getStrategyData";
 type Props = {};
+import { useAccount } from "wagmi";
 
 
 const columns: ColumnDef<Strategy>[] = [
@@ -66,38 +68,39 @@ const columns: ColumnDef<Strategy>[] = [
     header: "Actions",
     cell: ({ row }) => {
       return (
-      <div className="flex gap-2">
-        <Button variant= "outline" size= "icon">
-          <Pencil className="h-4 w-4"/>
-        </Button>
-        <Button variant="outline" size= "icon">
-          <XSquare className="h-4 w-4"/>
-        </Button>
-      </div>
+        <div className="flex gap-2">
+          <Button variant="outline" size="icon">
+            <Pencil className="h-4 w-4" />
+          </Button>
+          <Button variant="outline" size="icon">
+            <XSquare className="h-4 w-4" />
+          </Button>
+        </div>
       );
     },
   },
 ];
 
-const data: Strategy[] = [
-  // {
-  //   asset: "Arbitrum",
-  //   balance: "436.23",
-  //   actions: "2023-01-01",
-  //   strategy: "Tight ",
-  //   entryPrice: "$1000",
-  //   image: "https://ethereum-optimism.github.io/data/0xBTC/logo.png",
-  // }
-];
-
-const userStrategies = useScaffoldContractRead({
+const connectedAccount = useAccount().address || "";
+const { data: userContracts } = useScaffoldContractRead({
   contractName: "TrailMixManager",
   functionName: "getUserContracts",
-  args: [],
+  args: [connectedAccount],
 });
 
+const data: Strategy[] = [];
 
-export default function UsersPage({}: Props) {
+{
+  userContracts &&
+  [...userContracts].reverse().forEach((contractAddress: string) => {
+    const strategyData = getStrategyData({ contractAddress });
+    if (strategyData) {
+      data.push(strategyData);
+    }
+  })
+}
+
+export default function UsersPage({ }: Props) {
   return (
     <div className="flex flex-col gap-5  w-full">
       <PageTitle title="Your Strategies" />
